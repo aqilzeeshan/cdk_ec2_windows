28bf918e0fb96734391bd3f26f035c341e08745e
aqilzeeshan <aqilzeeshan@users.noreply.github.com>
2021-04-10 15:38:54 +0100

    changes

--- lib/cdk_ec2_windows-stack.ts
+++ lib/cdk_ec2_windows-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import autoscaling = require('@aws-cdk/aws-autoscaling');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
 
 export class CdkEc2WindowsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -43,25 +45,74 @@ export class CdkEc2WindowsStack extends cdk.Stack {
     userData.addCommands('iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))','install chocolatey')
     userData.addCommands('choco install googlechrome -y')
     userData.addCommands('choco install vscode -y')
+    
     userData.addCommands('Install-WindowsFeature Web-Server -IncludeManagementTools -IncludeAllSubFeature')
-    userData.addCommands('New-Item -ItemType directory -Path "C:\MyWebsite" -Force')
-    userData.addCommands('New-Item -ItemType File -Name "index.html" -Path "C:\MyWebsite\"')
-    userData.addCommands('Set-Content C:\MyWebsite\index.html "<html>Welcome to Our Site</html>"')
+    
+    //userData.addCommands('Remove-Website -Name "Default Web Site"')
+    //userData.addCommands('$defaultAppPools = @(".NET v2.0",".NET v2.0 Classic",".NET v4.5",".NET v4.5 Classic","Classic .NET AppPool","DefaultAppPool")')
+    //userData.addCommands('Foreach ($defaultAppPool in $defaultAppPools){IF (Test-path "IIS:\AppPools\$defaultAppPool"){Remove-WebAppPool -name $DefaultAppPool}}')
+
+    //userData.addCommands('New-Item -ItemType directory -Path "C:\MyWebsite" -Force')
+    //userData.addCommands('New-Item -ItemType File -Name "index.html" -Path "C:\MyWebsite\"')
+    //userData.addCommands('Set-Content C:\MyWebsite\index.html "<html>Welcome to Our Site</html>"')
+
     //userData.addCommands('New-IISSite -Name "MyWebsite" -PhysicalPath "C:\MyWebsite\" -BindingInformation "*:8088:"')
     //userData.addCommands('Get-IISSite')
 
     // Pick a Windows edition to use
     const windows = ec2.MachineImage.latestWindows(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE);
 
+    
     const webServer = new ec2.Instance(this, "WebInstance",{
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
       machineImage: windows,
       vpc: vpc,
       securityGroup:webserverSecurityGroup,
       vpcSubnets:subnetSelection,
-      keyName:'windows',
+      keyName:'AMI',
       userData:userData
     })
+    
 
+    /*
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      machineImage: windows,
+      vpc:vpc,
+      securityGroup:webserverSecurityGroup,
+      keyName:'windows',
+      minCapacity: 1,
+      desiredCapacity:1,
+      maxCapacity: 2,
+      //IP address for instances are so one can ssh and run stress test///// 
+      associatePublicIpAddress:true,
+      vpcSubnets:subnetSelection,
+      //////////////////////////////////////////////////////////////////////
+      userData:userData
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnCpuUtilization('KeepSpareCPU', {
+      targetUtilizationPercent: 50
+    });
+    
+    new cdk.CfnOutput(this, 'Loadbalancer DNS', { value: lb.loadBalancerDnsName });
+    */
   }
 }
